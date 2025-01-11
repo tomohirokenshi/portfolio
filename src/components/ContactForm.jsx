4056d8474431551292b7c87f343f074b48889259
Timothy Kenneth D. Dantic <timothykennethdantic@gmail.com>
2025-01-12 01:46:56 +0800

    Is this it?

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -21,11 +21,15 @@ const ContactForm = ({ onClose }) => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch("https://portfolio-mailer-lime.vercel.app", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
-      });
+      const response = await fetch(
+        "https://portfolio-mailer-lime.vercel.app/api/contact",
+        {
+          mode: "cors",
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(formData),
+        }
+      );
 
       if (response.ok) {
         console.log("Message sent successfully!");
