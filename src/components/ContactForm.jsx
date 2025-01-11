1f6d7cd76feb1531e31aed5418ac41a73b977c2c
Timothy Kenneth D. Dantic <timothykennethdantic@gmail.com>
2025-01-11 22:59:29 +0800

    Vercel Connection

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -22,7 +22,7 @@ const ContactForm = ({ onClose }) => {
 
     try {
       const response = await fetch(
-        "https://portfolio-mailer-8kxal39cu-tomohirokenshis-projects.vercel.app/api/contact",
+        "https://portfolio-mailer-if00kb7hn-tomohirokenshis-projects.vercel.app/api/contact",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
